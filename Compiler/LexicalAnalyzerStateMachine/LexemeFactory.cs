2d2bc6986e58e6dc9557c8068157e97aed5ecc26
Maxsman <dekxirus@gmail.com>
2022-11-04 23:23:57 +1000

    If structure was changed to case in LexemeFactory

--- Compiler/LexicalAnalyzerStateMachine/LexemeFactory.cs
+++ Compiler/LexicalAnalyzerStateMachine/LexemeFactory.cs
@@ -11,79 +11,72 @@ namespace Compiler.LexicalAnalyzerStateMachine
     {
         public ILexeme CreateLexemeByState(IState state, Coordinate coordinate, string source)
         {
-            if (state is DecimalEndState)
+            switch (state)
             {
-                var value = Convert.ToInt32(source, 10);
-                return new IntegerLexeme(coordinate, source, value);
-            }
-
-            if (state is HexEndState)
-            {
-                var valueForConvert = ReplaceString(source, "$", "0x");
-                var value = Convert.ToInt32(valueForConvert, 16);
-                return new IntegerLexeme(coordinate, source, value);
-            }
-
-            if (state is OctEndState)
-            {
-                var valueForConvert = ReplaceString(source, "&", "");
-                var value = Convert.ToInt32(valueForConvert, 8);
-                return new IntegerLexeme(coordinate, source, value);
-            }
-
-            if (state is BinEndState)
-            {
-                var valueForConvert = ReplaceString(source, "%", "");
-                var value = Convert.ToInt32(valueForConvert, 2);
-                return new IntegerLexeme(coordinate, source, value);
-            }
-
-            if (state is FloatEndState)
-            {
-                var tempCulture = Thread.CurrentThread.CurrentCulture;
-                Thread.CurrentThread.CurrentCulture = CultureInfo.CreateSpecificCulture("en-US");
-                var value = double.Parse(source);
-                Thread.CurrentThread.CurrentCulture = tempCulture;
-                return new FloatLexeme(coordinate, source, value);
-            }
-            
-            if (state is StringEndState)
-            {
-                var value = ReplaceString(source, "\'", "");
-                return new StringLexeme(coordinate, source, value);
-            }
-
-            if (state is CharEndState)
-            {
-                var value = ReplaceString(source, "\'", "");
-                return new CharLexeme(coordinate, source, value);
-            }
-
-            if (state is EndOfFileState)
-            {
-                return new EndOfFileLexeme(coordinate);
-            }
-
-            if (state is IdentifierEndState)
-            {
-                if (KeyWordsConstants.KeyWords.Contains(source))
+                case DecimalInteger:
                 {
-                    return new KeyWordLexeme(coordinate, source);
+                    var value = Convert.ToInt32(source, 10);
+                    return new IntegerLexeme(coordinate, source, value);
                 }
-                return new IdentifierLexeme(coordinate, source);
+                case HexEndState:
+                {
+                    var valueForConvert = ReplaceString(source, "$", "0x");
+                    var value = Convert.ToInt32(valueForConvert, 16);
+                    return new IntegerLexeme(coordinate, source, value);
+                }
+                case OctEndState:
+                {
+                    var valueForConvert = ReplaceString(source, "&", "");
+                    var value = Convert.ToInt32(valueForConvert, 8);
+                    return new IntegerLexeme(coordinate, source, value);
+                }
+                case BinEndState:
+                {
+                    var valueForConvert = ReplaceString(source, "%", "");
+                    var value = Convert.ToInt32(valueForConvert, 2);
+                    return new IntegerLexeme(coordinate, source, value);
+                }
+                case FloatEndState:
+                {
+                    var tempCulture = Thread.CurrentThread.CurrentCulture;
+                    Thread.CurrentThread.CurrentCulture = CultureInfo.CreateSpecificCulture("en-US");
+                    var value = double.Parse(source);
+                    Thread.CurrentThread.CurrentCulture = tempCulture;
+                    return new FloatLexeme(coordinate, source, value);
+                }
+                case StringEndState:
+                {
+                    var value = ReplaceString(source, "\'", "");
+                    return new StringLexeme(coordinate, source, value);
+                }
+                case CharEndState:
+                {
+                    var value = ReplaceString(source, "\'", "");
+                    return new CharLexeme(coordinate, source, value);
+                }
+                case EndOfFileState:
+                {
+                    return new EndOfFileLexeme(coordinate);
+                }
+                case IdentifierEndState:
+                {
+                    if (KeyWordsConstants.KeyWords.Contains(source))
+                    {
+                        return new KeyWordLexeme(coordinate, source);
+                    }
+                    return new IdentifierLexeme(coordinate, source);
+                }
+                case OperatorEndState:
+                {
+                    return new OperatorLexeme(coordinate, source);
+                }
+                case SeparatorEndState:
+                {
+                    return new SeparatorLexeme(coordinate, source);   
+                }
+                default:
+                    return new ErrorLexeme(coordinate, "IncorrectLexeme", source);
             }
-
-            if (state is SeparatorEndState)
-            {
-                return new SeparatorLexeme(coordinate, source);
-            }
-
-            if (state is OperatorEndState)
-            {
-                return new OperatorLexeme(coordinate, source);
-            }
-            
-            return new ErrorLexeme(coordinate, "IncorrectLexeme", source);
         }
 
         private string ReplaceString(string word, string oldString, string newString)
