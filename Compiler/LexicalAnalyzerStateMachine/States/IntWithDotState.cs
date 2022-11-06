adb4ca99ddc2c117d2c1aafb50402b41bfaf6c94
Maxsman <dekxirus@gmail.com>
2022-11-06 20:36:07 +1000

    .. Operator was added

---
+++ Compiler/LexicalAnalyzerStateMachine/States/IntWithDotState.cs
@@ -0,0 +1,41 @@
+﻿using Compiler.Constants;
+
+namespace Compiler.LexicalAnalyzerStateMachine.States;
+
+public class IntWithDotState : IState
+{
+    public IState GetNextState(int symbol)
+    {
+        if (symbol == LexemesSeparators.EndOfFile)
+        {
+            return new FloatEndState();
+        }
+
+        if (FloatConstants.FloatSymbol == (char) symbol)
+        {
+            return new RangeState();
+        }
+        
+        if (LexemesSeparators.VisibleSeparators.Contains((char)symbol))
+        {
+            return new FloatEndState();
+        }
+        
+        if (FloatConstants.NumbersFloat.Contains((char)symbol))
+        {
+            return new FloatState();
+        }
+
+        if (LexemesSeparators.ContainSymbol(symbol))
+        {
+            return new FloatEndState();
+        }
+        
+        if (OperatorConstants.Operators.Contains((char)symbol))
+        {
+            return new FloatEndState();
+        }
+
+        return new ErrorState("Incorrect lexeme, excepted 0-9 or some separator or '.', but " + (char)symbol + " found");
+    }
+}
---
+++ Compiler/LexicalAnalyzerStateMachine/States/RangeState.cs
@@ -0,0 +1,9 @@
+﻿namespace Compiler.LexicalAnalyzerStateMachine.States;
+
+public class RangeState : IState
+{
+    public IState GetNextState(int symbol)
+    {
+        return new RangeState();
+    }
+}
