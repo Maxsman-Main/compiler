6714b8a2dee3a3bde1c50c538dc84d5f4d729303
Maxsman <dekxirus@gmail.com>
2022-12-11 23:40:31 +1000

    Record Symbol was added

---
+++ Compiler/Parser/Semantic/SymbolRecord.cs
@@ -0,0 +1,11 @@
+﻿namespace Compiler.Parser.Semantic;
+
+public class SymbolRecord
+{
+    private SymbolTable _fields;
+
+    public SymbolRecord(SymbolTable fields)
+    {
+        _fields = fields;
+    }
+}
