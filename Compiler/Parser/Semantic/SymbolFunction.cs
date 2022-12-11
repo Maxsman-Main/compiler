bda474b9074f16e754b39c00996a537a74c7e084
Maxsman <dekxirus@gmail.com>
2022-12-11 23:49:53 +1000

    SymbolProcedure and SymbolFunction were added

---
+++ Compiler/Parser/Semantic/SymbolFunction.cs
@@ -0,0 +1,13 @@
+﻿using Compiler.Parser.Tree;
+
+namespace Compiler.Parser.Semantic;
+
+public class SymbolFunction : SymbolProcedure
+{
+    private SymbolType _returnType;
+    
+    public SymbolFunction(string name, SymbolTable parameters, SymbolTable locals, CompoundStatement body, SymbolType returnType) : base(name, parameters, locals, body)
+    {
+        _returnType = returnType;
+    }
+}
---
+++ Compiler/Parser/Semantic/SymbolProcedure.cs
@@ -0,0 +1,17 @@
+﻿using Compiler.Parser.Tree;
+
+namespace Compiler.Parser.Semantic;
+
+public class SymbolProcedure : Symbol
+{
+    private SymbolTable _parameters;
+    private SymbolTable _locals;
+    private CompoundStatement _body;
+
+    public SymbolProcedure(string name, SymbolTable parameters, SymbolTable locals, CompoundStatement body) : base(name)
+    {
+        _parameters = parameters;
+        _locals = locals;
+        _body = body;
+    }
+}
