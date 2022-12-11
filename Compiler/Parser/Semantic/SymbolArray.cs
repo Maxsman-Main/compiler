ee0fce39d0bc92b94149f3657b1622c2ecde558b
Maxsman <dekxirus@gmail.com>
2022-12-11 23:32:35 +1000

    Array was added

---
+++ Compiler/Parser/Semantic/SymbolArray.cs
@@ -0,0 +1,15 @@
+﻿namespace Compiler.Parser.Semantic;
+
+public class SymbolArray
+{
+    private SymbolType _itemsType;
+    private int _leftBound;
+    private int _rightBound;
+
+    public SymbolArray(SymbolType type, int leftBound, int rightBound)
+    {
+        _itemsType = type;
+        _leftBound = leftBound;
+        _rightBound = rightBound;
+    }
+}
