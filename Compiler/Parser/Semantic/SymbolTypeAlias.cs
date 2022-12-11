4f9999b9aa90e1a27a2e53efd84ddfa01f518037
Maxsman <dekxirus@gmail.com>
2022-12-11 23:42:02 +1000

    SymbolTypeAlias was added

---
+++ Compiler/Parser/Semantic/SymbolTypeAlias.cs
@@ -0,0 +1,11 @@
+﻿namespace Compiler.Parser.Semantic;
+
+public class SymbolTypeAlias
+{
+    private SymbolType _original;
+
+    public SymbolTypeAlias(SymbolType original)
+    {
+        _original = original;
+    }
+}
